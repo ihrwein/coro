b1c0fb6cd79c547008074c51cec800ecd7c6855b
Tibor Benke <ihrwein@gmail.com>
2017-09-02 21:30:37 +0200

    refact: rename methods (breaking change)
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -10,14 +10,18 @@ thread_local! {
     static CORE: RefCell<Core> = RefCell::new(Core::new().unwrap());
 }
 
-pub fn run_scoped<I, E, F: Future<Item = I, Error = E>>(f: F) -> Result<I, E> {
+// Run the f Future on this thread's event loop and wait for its completion, then
+// return with its result.
+pub fn run<I, E, F: Future<Item = I, Error = E>>(f: F) -> Result<I, E> {
     CORE.with(|k| {
         let mut core = k.borrow_mut();
         core.run(f)
     })
 }
 
-pub fn run<F: 'static + Future<Item = (), Error = ()>>(f: F) {
+// Go runs the f Future on this thread's event loop and immediately returns
+// back to the caller.
+pub fn go<F: 'static + Future<Item = (), Error = ()>>(f: F) {
     CORE.with(|k| {
         let core = k.borrow_mut();
         let handle = core.handle();
@@ -25,6 +29,7 @@ pub fn run<F: 'static + Future<Item = (), Error = ()>>(f: F) {
     });
 }
 
+// Handler returns a handler to this handler's event loop.
 pub fn handler() -> Handle {
     CORE.with(|k| k.borrow().handle())
 }
